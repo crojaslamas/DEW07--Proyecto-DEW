e3a55741603d60175cc8fbec69eb54e972f53770
Marlon <Marlon171199@gmail.com>
2026-08-13 12:16:45 -0500

    Agregue menu colapsable

--- CompuStore/CompuStore/src/componentes/Menu.jsx
+++ CompuStore/CompuStore/src/componentes/Menu.jsx
@@ -1,34 +1,41 @@
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { CarritoContext } from "../context/CarritoContext";
-import { Reloj } from './Reloj'; // Importas el reloj
-
-
+import { Reloj } from './Reloj';
 
 function Menu() {
-    const { cantidad } = useContext(CarritoContext);
-
+  const { cantidad } = useContext(CarritoContext);
+  const [open, setOpen] = useState(false); // estado para abrir/cerrar menú
 
   return (
     <header className="menu">
-
+      {/* Logo */}
       <NavLink to="/" className="logo-titulo">
         <h2 className="logo">COMPU-STORE</h2>
       </NavLink>
 
-      <nav>
+      {/* Botón hamburguesa */}
+      <button 
+        className={`hamburguesa ${open ? "open" : ""}`} 
+        onClick={() => setOpen(!open)}
+        aria-label="Toggle navigation"
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Menú colapsable */}
+      <nav className={`nav-links ${open ? "show" : ""}`}>
         <NavLink to="/">Inicio</NavLink>
         <NavLink to="/productos">Productos</NavLink>
         <NavLink to="/contacto">Contacto</NavLink>
         <NavLink to="/carrito" className="icono-carrito">
-        <img src="img/shop.png" alt="Carrito" />
-         <span>{cantidad}</span>
+          <img src="img/shop.png" alt="Carrito" />
+          <span>{cantidad}</span>
         </NavLink>
-       
         <Reloj />
       </nav>
-
-
     </header>
   );
 }
